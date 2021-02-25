3918055ded1122cbfdcd48666aec3ea391623706
Jesse Smart <jsmartdev@protonmail.com>
2021-02-24 21:46:16 -0500

    Complete conditional statements

---
+++ 7-Javascript-Programming/04-Conditional Statements/script.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+let direction = "Backwards";
+
+if(direction == "Left") {
+    console.log("Red Planet");
+}
+
+else if(direction == "Right") {
+    console.log("Purple Planet");
+}
+
+else if(direction == "Backwards") {
+    console.log("Back Home");
+}
+
+else {
+    console.log("Fatal Crash")
+}
