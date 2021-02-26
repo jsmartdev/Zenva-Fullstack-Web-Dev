6a3442e414cc0d7475ad1a56284d46d2263fc7a4
Jesse Smart <jsmartdev@protonmail.com>
2021-02-26 16:42:22 -0500

    Complete Iterating Over Arrays lesson

---
+++ 7-Javascript-Programming/29-Iterating-Over-Arrays/iterating.js
@@ -0,0 +1,52 @@
+/* eslint-disable */
+
+let scores = [10, 20, 10];
+
+/* let i = 0;
+while(i < scores.length) {
+    scores[i]++;
+    i++;
+}
+
+console.log(scores); */
+
+console.log(scores);
+
+for(i = 0; i < scores.length; i++) {
+    scores[i] ++;
+}
+
+console.log(scores);
+
+scores.forEach(function(entry, index){
+    scores[index]++;
+});
+
+console.log(scores);
+
+let catalog = [
+    {
+        title: "Javscript for Beginners",
+        author: "Zenva",
+        copies: 1
+    },
+    {
+        title: "HTML for Beginners",
+        author: "Zenva",
+        copies: 1
+    },
+    {
+        title: "CSS for Beginners",
+        author: "XYZ",
+        copies: 1
+    }];
+
+    catalog.forEach(function(entry) {
+        console.log(entry);
+        if(entry.author == "Zenva") {
+            entry.copies++;
+        }
+    });
+
+    console.log(catalog);
+
