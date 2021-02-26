a27150a64cf7716ef5e5b3b692ccb7956917dfef
Jesse Smart <jsmartdev@protonmail.com>
2021-02-26 15:29:24 -0500

    Complet For Loop lesson

---
+++ 7-Javascript-Programming/25-For-Loops/forloops.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+/*let population = 100;
+let years = 0;
+
+while(years < 10) {
+    population *= 1.05;
+    years++;
+}
+
+console.log(population, years) */
+
+let population = 100;
+
+for(i = 0; i < 10; i++) {
+    population *= 1.05;
+}
+
+console.log(population);
