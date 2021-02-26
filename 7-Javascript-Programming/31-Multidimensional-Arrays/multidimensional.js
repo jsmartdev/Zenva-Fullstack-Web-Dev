c20e42a1668662664afa2fa891eb1a537bd91764
Jesse Smart <jsmartdev@protonmail.com>
2021-02-26 17:04:01 -0500

    Complete Multidimensional Arrays lesson

---
+++ 7-Javascript-Programming/31-Multidimensional-Arrays/multidimensional.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+let a =[[1, 2, 3], [4, 5, 6]];
+
+console.log(a[1][0]);
+
+a[0][2] = 8;
+
+console.log(a[0][2]);
+
+let terrains = [
+    ["desert", "desert", "grass", "grass"],
+    ["desert", "grass", "water", "grass"],
+    ["grass", "grass", "water", "water"],
+    ["grass", "grass", "grass", "grass"]
+];
+
+console.log(terrains);
